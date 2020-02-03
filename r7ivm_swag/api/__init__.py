1bd56da682add433a7d05a7982fef83968e0ec19
justinlute <justinlute@gmail.com>
2020-02-03 08:31:09 -0500

    updated swagger_client path

--- r7ivm_swag/api/__init__.py
+++ r7ivm_swag/api/__init__.py
@@ -3,23 +3,23 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
-from swagger_client.api.administration_api import AdministrationApi
-from swagger_client.api.asset_api import AssetApi
-from swagger_client.api.asset_discovery_api import AssetDiscoveryApi
-from swagger_client.api.asset_group_api import AssetGroupApi
-from swagger_client.api.credential_api import CredentialApi
-from swagger_client.api.policy_api import PolicyApi
-from swagger_client.api.policy_override_api import PolicyOverrideApi
-from swagger_client.api.remediation_api import RemediationApi
-from swagger_client.api.report_api import ReportApi
-from swagger_client.api.root_api import RootApi
-from swagger_client.api.scan_api import ScanApi
-from swagger_client.api.scan_engine_api import ScanEngineApi
-from swagger_client.api.scan_template_api import ScanTemplateApi
-from swagger_client.api.site_api import SiteApi
-from swagger_client.api.tag_api import TagApi
-from swagger_client.api.user_api import UserApi
-from swagger_client.api.vulnerability_api import VulnerabilityApi
-from swagger_client.api.vulnerability_check_api import VulnerabilityCheckApi
-from swagger_client.api.vulnerability_exception_api import VulnerabilityExceptionApi
-from swagger_client.api.vulnerability_result_api import VulnerabilityResultApi
+from r7ivm_swag.api.administration_api import AdministrationApi
+from r7ivm_swag.api.asset_api import AssetApi
+from r7ivm_swag.api.asset_discovery_api import AssetDiscoveryApi
+from r7ivm_swag.api.asset_group_api import AssetGroupApi
+from r7ivm_swag.api.credential_api import CredentialApi
+from r7ivm_swag.api.policy_api import PolicyApi
+from r7ivm_swag.api.policy_override_api import PolicyOverrideApi
+from r7ivm_swag.api.remediation_api import RemediationApi
+from r7ivm_swag.api.report_api import ReportApi
+from r7ivm_swag.api.root_api import RootApi
+from r7ivm_swag.api.scan_api import ScanApi
+from r7ivm_swag.api.scan_engine_api import ScanEngineApi
+from r7ivm_swag.api.scan_template_api import ScanTemplateApi
+from r7ivm_swag.api.site_api import SiteApi
+from r7ivm_swag.api.tag_api import TagApi
+from r7ivm_swag.api.user_api import UserApi
+from r7ivm_swag.api.vulnerability_api import VulnerabilityApi
+from r7ivm_swag.api.vulnerability_check_api import VulnerabilityCheckApi
+from r7ivm_swag.api.vulnerability_exception_api import VulnerabilityExceptionApi
+from r7ivm_swag.api.vulnerability_result_api import VulnerabilityResultApi
